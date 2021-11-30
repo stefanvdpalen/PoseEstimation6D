2fa7394b4d7dffb5f48e45c7155c5b1c338f8639
stefanvdpalen <stefanvdpalen@hotmail.com>
2021-11-30 17:04:39 +0000

    Initialisation

--- YOLO/YOLO_txt_format_creator.py
+++ YOLO/YOLO_txt_format_creator.py
@@ -10,16 +10,16 @@ for count, fname in enumerate(images):
     contours, _ = cv2.findContours(im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
-        if w > 900 or h > 900: continue
-        cv2.rectangle(im_bw, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(im_bw, 'Moth Detected', (x + w + 10, y + h), 0, 0.3, (0, 255, 0))
-        cv2.imshow("Show", im_bw)
-        cv2.waitKey()
+        if w > 0.9*width_im or h > 0.9*height_im: continue
         x_centre = (x + w / 2) / width_im
         y_centre = (y + h / 2) / height_im
-        width_YOLO = x / width_im
-        height_YOLO = y / height_im
+        width_YOLO = w / width_im
+        height_YOLO = h / height_im
         String_print = ["0 ", str(x_centre), " ", str(y_centre), " ", str(width_YOLO), " ", str(height_YOLO)]
+        cv2.rectangle(im_bw, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(im_bw, "Box detected", (x + w + 10, y + h), 0, 0.3, (0, 255, 0))
+        #cv2.imshow("Show", im_bw)
+        #cv2.waitKey()
         new_filename = fname
         new_filename = new_filename[:-3]
         filetype = "txt"
